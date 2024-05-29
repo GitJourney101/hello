5b1102289d94d21878980bce1e245d11a914e4da
Mahendra <mahendra@example.com>
2024-05-29 14:06:30 -0400

    first commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+echo "Hellow word"
+echo "The build ID for this job is: ${BUILD_ID}"
+echo "The build URL for this job is: ${BUILD_URL}"
+echo "Present Directory:" `pwd`
+ls -altr
+echo "1234" > test1.txt
+ls -altr
