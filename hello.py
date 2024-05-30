155a0a887ae28a11e680e2844475ab3024abb7d2
GitJourney101 <devops101@yahoo.com>
2024-05-29 21:33:26 -0400

    fourth commit

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 print ("Hellow word")
 print ("The build ID for this job is: ${BUILD_ID}")
 print ("The build URL for this job is: ${BUILD_URL}")
-print ("Present Directory:" `pwd`)
+pwd
 ls -altr
-echo  "1234" > test1.txt
+touch test1.txt
 ls -altr
 print ("Hellow new Git repo")
